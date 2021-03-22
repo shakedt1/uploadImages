d19335edb0471ad1a4ba2914a81baa53ec9e8d10
shakedt1 <49534411+shakedt1@users.noreply.github.com>
2021-03-22 15:33:54 +0200

    Add project files.

---
+++ uploadImages/MainActivity.cs
@@ -0,0 +1,73 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+using Android.Net;
+using System.IO;
+using static Android.Provider.MediaStore;
+using Android.Database;
+using System.Net.Http;
+
+namespace uploadImages
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            Intent photoPickerIntent = new Intent(Intent.ActionGetContent);
+            photoPickerIntent.SetType("image/*");
+            StartActivityForResult(photoPickerIntent, 1);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            if (requestCode == 1)
+            {
+                var uri = data.Data;
+                var path = GetPathToImage(uri);
+
+
+            }
+        }
+
+        public string GetPathToImage(Uri uri)
+        {
+            string doc_id = "";
+            using (var c1 = ContentResolver.Query(uri, null, null, null, null))
+            {
+                c1.MoveToFirst();
+                string document_id = c1.GetString(0);
+                doc_id = document_id.Substring(document_id.LastIndexOf(":") + 1);
+            }
+
+            string path = null;
+
+            // The projection contains the columns we want to return in our query.
+            string selection = Images.Media.InterfaceConsts.Id + " =? ";
+            using (var cursor = ContentResolver.Query(Images.Media.ExternalContentUri, null, selection, new string[] { doc_id }, null))
+            {
+                if (cursor == null) return path;
+                var columnIndex = cursor.GetColumnIndexOrThrow(Images.Media.InterfaceConsts.Data);
+                cursor.MoveToFirst();
+                path = cursor.GetString(columnIndex);
+            }
+            return path;
+        }
+
+    }
+}
